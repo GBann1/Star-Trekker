4f9898e497b4f99fadd5fb5a3b238a97c3125f14
OWEN LANG <owenlang66@gmail.com>
2023-08-18 15:38:35 -0700

    commit

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 const Users = require('../controllers/user.controller');
-const { authenticate } = require('../config/jwt.config');
+const { authenticate } = require('../configs/jwt.config');
 module.exports = app => {
     app.post("/api/register", Users.register);
     app.post("/api/login", Users.login);
