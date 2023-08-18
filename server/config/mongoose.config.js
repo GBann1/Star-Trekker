e26bb4920b4e3f04927f8a6e3bd645ccce28cbd8
OWEN LANG <owenlang66@gmail.com>
2023-08-18 13:34:11 -0700

    config

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,9 +4,6 @@ const mongoose = require("mongoose");
 // 2. create URI variable 
 const uri = process.env.URI;
 
-// just to test if it's getting through to the server
-console.log(uri); 
-
 mongoose.connect(uri)
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
