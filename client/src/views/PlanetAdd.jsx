d1a61cb3b6574fc97cb9e173df1e9e16cacfaf46
Albert <Albertyang1112@gmail.com>
2023-08-19 16:19:52 -0700

    add planet

--- client/src/views/PlanetAdd.jsx
+++ client/src/views/PlanetAdd.jsx
@@ -1,10 +1,12 @@
+import axios from "axios";
 import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom";
 
 const PlanetAdd = () => {
 
     const [planet, setPlanet] = useState({
         planetName: "",
-        rotationPeriod: 0,
+        rotationPeriod: "",
         orbitalPeriod: 0,
         diameter: 0,
         climate: "",
@@ -14,18 +16,53 @@ const PlanetAdd = () => {
         population: 0
         //coordinates: [x,y,z,r]
     })
+
+    const nav = useNavigate();
     
     const submitHandler = (e) => {
         e.preventDefault();
 
+        axios.post("http://localhost:8000/api/planets", {planet})
+            .then(res => {
+                setPlanet.planetName("");
+                setPlanet.rotationPeriod("");
+                setPlanet.orbitalPeriod();
+                setPlanet.diameter();
+                setPlanet.className("");
+                setPlanet.gravity("");
+                setPlanet.terrain("");
+                setPlanet.surfaceWater();
+                setPlanet.population();
 
+                nav("/");
+            })
     }
     
     return (
-        <form onSubmit={submitHandler}>
-            <label>Name:</label>
-            <input type="text" />
-        </form>
+        <div>
+            <h1 className='m-auto'>New Planet Information</h1>
+            <form onSubmit={submitHandler}>
+                <label>Name:</label>
+                <input type="text" value={planet.planetName} onChange={setPlanet.planetName(e.target.value)}/>
+                <label>Rotation period:</label>
+                <input type="text" value={planet.rotationPeriod} onChange={setPlanet.rotationPeriod(e.target.value)}/>
+                <label>Orbital Period:</label>
+                <input type="number" value={planet.orbitalPeriod} onChange={setPlanet.orbitalPeriod(e.target.value)}/>
+                <label>Diameter:</label>
+                <input type="number" value={planet.diameter} onChange={setPlanet.diameter(e.target.value)}/>
+                <label>Climate:</label>
+                <input type="text" value={planet.climate} onChange={setPlanet.className(e.target.value)}/>
+                <label>Gravity</label>
+                <input type="text" value={planet.gravity} onChange={setPlanet.gravity(e.target.value)}/>
+                <label>Terrain:</label>
+                <input type="text" value={planet.terrain} onChange={setPlanet.terrain(e.target.value)}/>
+                <label>Surface Water:</label>
+                <input type="number" value={planet.surfaceWater} onChange={setPlanet.surfaceWater(e.target.value)}/>
+                <label>Population</label>
+                <input type="number" value={planet.population} onChange={setPlanet.population(e.target.value)}/>
+                <input type="submit" value="Log Plnaet"/>
+            </form>
+        </div>
     )
 }
 
