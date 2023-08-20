270b0a7953d1912a4bbb65659d19e222bc8e401b
GBann <GTBannon@protonmail.com>
2023-08-20 17:09:00 -0600

    test

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Dashboard = () => {
+    //get axios call for their past travels
+    const {id} = useParams();
+    const [userTravels, setUserTravels] = useState()
+    axios.get(`http://localhost:8000/api/user/travels/${id}`)
+        .then(res=>{
+            const data = res.data
+            setUserTravels(data)
+        })
+        .catch(err=>console.log(err))
     return (
         <div>
-
+            <h1>Your Travels</h1>
+            {userTravels.map((trip,idx)=>{
+                return(
+                    <div key={idx}>
+                        <h1>hello</h1>
+                    </div>
+                )
+            })}
         </div>
     )
 }
