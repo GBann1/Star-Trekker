2f12c39f3e2cfa483309b560527a268755dea51e
GBann <GTBannon@protonmail.com>
2023-08-20 17:01:14 -0600

    made dashboard view & link in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Routes, Route, Link} from 'react-router-dom';
 import LoginPage from './views/LoginPage';
 import LandingPage from './components/LandingPage';
+import Dashboard from './views/Dashboard';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/login_or_register' element={<LoginPage/>}/>
+        <Route path='/dashboard/:id' element={<Dashboard/>}/>
       </Routes>
     </div>
   );
