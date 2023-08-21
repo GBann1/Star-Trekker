dba11359a5d51bb261f2c85c18413451cb4bfb06
GBann <GTBannon@protonmail.com>
2023-08-21 15:06:07 -0600

    take 2?

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import LandingPage from './components/LandingPage';
 import Dashboard from './views/Dashboard';
 import UserDashboard from './views/UserDashboard';
 import GalaxyChart from './components/GalaxyChart';
+import Page404 from './views/Page404';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
+        <Route path='/404' element={<Page404/>}/>
         <Route path='/login_or_register' element={<LoginPage/>}/>
         {/* <Route path='/dashboard/:id' element={<UserDashboard/>}/> */}
         <Route path='/dashboard/' element={<UserDashboard/>}/>
