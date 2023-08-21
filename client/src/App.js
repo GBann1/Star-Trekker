d29b77a06af3362abb8b24880cf8505d88ee654e
EricLiu27 <eric.liu82794@gmail.com>
2023-08-21 17:30:23 -0400

    iron-session

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
-import {Routes, Route, Link} from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './views/LoginPage';
 import LandingPage from './components/LandingPage';
 import Dashboard from './views/Dashboard';
 import UserDashboard from './views/UserDashboard';
 import GalaxyChart from './components/GalaxyChart';
 import Page404 from './views/Page404';
+import AppContext from './libs/context';
+import { useState } from "react";
+
+
 function App() {
+  const [loggedUser, setLoggedUser] = useState();
   return (
-    <div className="App">
+    <AppContext.Provider value={{ loggedUser, setLoggedUser }}>
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/404' element={<Page404/>}/>
-        <Route path='/login_or_register' element={<LoginPage/>}/>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/404' element={<Page404 />} />
+        <Route path='/login_or_register' element={<LoginPage />} />
         {/* <Route path='/dashboard/:id' element={<UserDashboard/>}/> */}
-        <Route path='/dashboard/' element={<UserDashboard/>}/>
-        <Route path='/see_history' element={<Dashboard/>}/>
-        <Route path='/delete/GalaxyChart' element={<GalaxyChart/>}/>
+        <Route path='/dashboard/' element={<UserDashboard />} />
+        <Route path='/see_history' element={<Dashboard />} />
+        <Route path='/delete/GalaxyChart' element={<GalaxyChart />} />
 
       </Routes>
-    </div>
+    </AppContext.Provider>
   );
 }
 
