16adc81e9ee01f2ae6e7495f46b3640fb9f8dd52
EricLiu27 <eric.liu82794@gmail.com>
2023-08-22 13:14:46 -0400

    Dashboard Nav

--- client/src/views/UserDashboard.jsx
+++ client/src/views/UserDashboard.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import GalaxyChart from '../components/GalaxyChart';
 import axios from 'axios'
 import Carousel from 'react-bootstrap/Carousel';
 import image from "../images/logo.png"
-import { useNavigate, useParams } from 'react-router-dom'
 
 const UserDashboard = () => {
     const [planetList, setPlanetList] = useState([]);
@@ -22,9 +21,9 @@ const UserDashboard = () => {
             .then(res => setPlanetList(res.data))
             .catch(err => console.log(err))
 
-        axios.get(`http://localhost:8000/api/users/${id}`)
-            .then(res => setFirstName(res.data))
-            .catch()
+        // axios.get(`http://localhost:8000/api/users/${id}`)
+        //     .then(res => setFirstName(res.data))
+        //     .catch()
     }, [])
 
 
@@ -36,13 +35,24 @@ const UserDashboard = () => {
                         <img style={{ height: 50, width: 50 }} className='img-fluid' src={`${image}`} alt="logo" />
                     </div>
                     <div>
-                        <h1 className='align-item center-center'>Hello {id}</h1>
+                        <h1 className='align-item center-center '>Star Trekkers</h1>
                     </div>
-                    <div className='align-item center-center'>
-
+                    <div className='align-item center-center '>
+                        <ul className='navbar-nav me-auto '>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Welcome {id} </a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Start Your Journey</a>
+                                    <a class="dropdown-item" href="#">View Profile</a>
+                                    <a class="dropdown-item" href="#">Add an Entity</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">LOGOUT</a>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </nav>
+            </nav >
             <div>
                 <p>Display Planet detail cards (carousel)</p>
                 <p>Post reviews of each planet</p>
@@ -67,7 +77,7 @@ const UserDashboard = () => {
                     </Carousel>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
