745e2c10be862024095e09b8707f78b6bc650559
Albert <Albertyang1112@gmail.com>
2023-08-24 15:36:49 -0700

    useContext

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
                                 <div className="dropdown-menu">
                                     <Link className="dropdown-item" to={`/dashboard`}>Dashboard</Link>
                                     <Link className="dropdown-item" to="/trip">Start Your Trip</Link>
-                                    <Link className="dropdown-item" to={`/see_history/${id}`}>View Profile</Link>
+                                    <Link className="dropdown-item" to={`/see_history/${userID}`}>View Profile</Link>
                                     <Link className="dropdown-item" to="/entity">Add an Entity</Link>
                                     <div className="dropdown-divider"></div>
                                     <button className="dropdown-item" onClick={logoutHandler}>LOGOUT</button>
