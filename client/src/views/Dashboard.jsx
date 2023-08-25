051717fb80205a279da8f149efd4fda0dc7efe7f
GBann <GTBannon@protonmail.com>
2023-08-25 01:39:35 -0600

    added button

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import DisplayTrips from '../components/DisplayTrips';
-import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import addIcon from '../images/plus-circle.svg'
 
+
 const Dashboard = () => {
-    
+    const navigate = useNavigate();
     return (
         <div>
             <Navbar />
             <h1 style={{margin:'10px'}}>View Your Travels:</h1>
             <DisplayTrips/>
-            <div className='text-align-right'>
-                <Link to='/trip' ><img src={addIcon}></img></Link>
+            <div style={{textAlign:'right', marginRight:'1%'}}>
+                {/* <Link to='/trip' style={{backgroundColor:'rgb(253, 246, 227)', border:'2px solid rgb(131, 148, 150)', borderRadius:'15%', padding:'px'}}><img src={addIcon}></img></Link> */}
+                <img src={addIcon} onClick={()=>navigate('/trip')} style={{backgroundColor:'rgb(253, 246, 227)', border:'2px solid rgb(131, 148, 150)', borderRadius:'50%', height:'40px'}}></img>
             </div>
         </div>
     )
