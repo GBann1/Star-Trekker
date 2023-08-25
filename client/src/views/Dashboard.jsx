2891c63c12c2b44fb74c43ad8cf7267d7516d39d
GBann <GTBannon@protonmail.com>
2023-08-25 01:39:56 -0600

    cleaned up file

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -4,7 +4,6 @@ import DisplayTrips from '../components/DisplayTrips';
 import {useNavigate} from 'react-router-dom';
 import addIcon from '../images/plus-circle.svg'
 
-
 const Dashboard = () => {
     const navigate = useNavigate();
     return (
@@ -13,7 +12,6 @@ const Dashboard = () => {
             <h1 style={{margin:'10px'}}>View Your Travels:</h1>
             <DisplayTrips/>
             <div style={{textAlign:'right', marginRight:'1%'}}>
-                {/* <Link to='/trip' style={{backgroundColor:'rgb(253, 246, 227)', border:'2px solid rgb(131, 148, 150)', borderRadius:'15%', padding:'px'}}><img src={addIcon}></img></Link> */}
                 <img src={addIcon} onClick={()=>navigate('/trip')} style={{backgroundColor:'rgb(253, 246, 227)', border:'2px solid rgb(131, 148, 150)', borderRadius:'50%', height:'40px'}}></img>
             </div>
         </div>
